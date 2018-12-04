18508d7103871b0025feeac92b0185aa97444181
SunShine <07033320a@gmail.com>
2018-12-04 17:09:56 +0800

    Update hasPermission.js

--- vue/src/utils/hasPermission.js
+++ vue/src/utils/hasPermission.js
@@ -4,3 +4,9 @@ export function hasPermission(permission) {
   let myPermissions = store.getters.permissions;
   return myPermissions.indexOf(permission) > -1;
 }
+
+export function isAdmin(roleName) {
+  let myRole = store.getters.role;
+  return myRole.indexOf(roleName) > -1;
+}
+
