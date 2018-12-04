5168ae0e6b31434ac1f00830458254ceef35766a
SunShine <07033320a@gmail.com>
2018-12-04 17:09:39 +0800

    Update main.js

--- vue/src/main.js
+++ vue/src/main.js
@@ -8,12 +8,13 @@ import store from './store'
 import '@/icons' // icon
 import '@/permission' // 权限
 import {default as api} from './utils/api'
-import {hasPermission} from "./utils/hasPermission";
+import {hasPermission, isAdmin} from "./utils/hasPermission";
 import i18n from './lang' // Internationalization
 Vue.use(ElementUI, {  i18n: (key, value) => i18n.t(key, value)})
 Vue.prototype.api = api
 //全局的常量
 Vue.prototype.hasPerm = hasPermission
+Vue.prototype.isAdmin = isAdmin
 //生产环境时自动设置为 false 以阻止 vue 在启动时生成生产提示。
 Vue.config.productionTip = (process.env.NODE_ENV != 'production');
 Vue.prototype.$bus = new Vue();
