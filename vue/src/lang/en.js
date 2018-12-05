a2cb5481910702f55221c98d7c7baac70ad6ff37
SunShine <07033320a@gmail.com>
2018-12-05 10:23:38 +0800

    Update en.js

--- vue/src/lang/en.js
+++ vue/src/lang/en.js
@@ -4,10 +4,39 @@ export default {
     wed: 'Wed',
     thu: 'Thu',
   },
+
+  teacher:{
+    courseName: 'Course Name(Detail)',
+    studentNum: 'Student Num',
+    grade: 'Grade',
+    tuition: 'Tuition',
+    tuitionOption1: '(Person*Lesson)',
+    tuitionOption2: '(Lesson)',
+    tuitionOption3: '(Term)',
+    tuitionFree: 'Free',
+    courseDate: 'Course Date',
+    updateDate: 'Update Time',
+    teacherName: 'Teacher',
+
+    step1:'Common',
+    step2:'Additional',
+    step3:'Preview',
+    courseNameNoDetail: 'Course Name',
+    courseType: 'Course Type',
+    brief: 'Brief',
+    attachment: 'Attachment',
+    uploadTip1: 'Drag the file here, or ',
+    uploadTip2: 'Click to upload',
+    uploadTip3: 'Upload only PDF / Word files, and no more than 3Mb',
+    prevStep: 'Previous',
+    nextStep: 'Next',
+    finishStep: 'Finish',
+
+  },
   common:{
     loading: 'Loading...',
     checkAll: 'ALL',
-  deleteSuccess: 'Delete successfully'
+    deleteSuccess: 'Delete successfully'
   },
   route: {
     dashboard: 'Dashboard',
@@ -36,76 +65,27 @@ export default {
     navLabel: 'Course period',
     formSelectPeriod: 'Course period'
   },
-  documentation: {
-    documentation: 'Documentation',
-    github: 'Github Repository'
-  },
-  permission: {
-    roles: 'Your roles',
-    switchRoles: 'Switch roles'
-  },
-  guide: {
-    description: 'The guide page is useful for some people who entered the project for the first time. You can briefly introduce the features of the project. Demo is based on ',
-    button: 'Show Guide'
-  },
-  components: {
-    documentation: 'Documentation',
-    tinymceTips: 'Rich text editor is a core part of management system, but at the same time is a place with lots of problems. In the process of selecting rich texts, I also walked a lot of detours. The common rich text editors in the market are basically used, and the finally chose Tinymce. See documentation for more detailed rich text editor comparisons and introductions.',
-    dropzoneTips: 'Because my business has special needs, and has to upload images to qiniu, so instead of a third party, I chose encapsulate it by myself. It is very simple, you can see the detail code in @/components/Dropzone.',
-    stickyTips: 'when the page is scrolled to the preset position will be sticky on the top.',
-    backToTopTips1: 'When the page is scrolled to the specified position, the Back to Top button appears in the lower right corner',
-    backToTopTips2: 'You can customize the style of the button, show / hide, height of appearance, height of the return. If you need a text prompt, you can use element-ui el-tooltip elements externally',
-    imageUploadTips: 'Since I was using only the vue@1 version, and it is not compatible with mockjs at the moment, I modified it myself, and if you are going to use it, it is better to use official version.'
-  },
   table: {
-    dynamicTips1: 'Fixed header, sorted by header order',
-    dynamicTips2: 'Not fixed header, sorted by click order',
-    dragTips1: 'The default order',
-    dragTips2: 'The after dragging order',
     title: 'Title',
-    importance: 'Imp',
     type: 'Type',
     remark: 'Remark',
     search: 'Search',
     add: 'Add',
     export: 'Export',
-    reviewer: 'reviewer',
     id: 'ID',
     date: 'Date',
-    author: 'Author',
-    readings: 'Readings',
     status: 'Status',
     actions: 'Actions',
     edit: 'Edit',
     publish: 'Publish',
     draft: 'Draft',
     delete: 'Delete',
+    manage: 'Manage',
+    deleteConfirm: 'To confirm the deletion ?',
     cancel: 'Cancel',
     confirm: 'Confirm'
   },
-  errorLog: {
-    tips: 'Please click the bug icon in the upper right corner',
-    description: 'Now the management system are basically the form of the spa, it enhances the user experience, but it also increases the possibility of page problems, a small negligence may lead to the entire page deadlock. Fortunately Vue provides a way to catch handling exceptions, where you can handle errors or report exceptions.',
-    documentation: 'Document introduction'
-  },
   excel: {
     export: 'Export',
-    selectedExport: 'Export Selected Items',
-    placeholder: 'Please enter the file name(default excel-list)'
-  },
-  zip: {
-    export: 'Export',
-    placeholder: 'Please enter the file name(default file)'
-  },
-  theme: {
-    change: 'Change Theme',
-    documentation: 'Theme documentation',
-    tips: 'Tips: It is different from the theme-pick on the navbar is two different skinning methods, each with different application scenarios. Refer to the documentation for details.'
-  },
-  tagsView: {
-    refresh: 'Refresh',
-    close: 'Close',
-    closeOthers: 'Close Others',
-    closeAll: 'Close All'
   }
 }
