206cdf9a59739dd4a71da1b7c84b8da490c926fa
SunShine <07033320a@gmail.com>
2018-12-13 15:03:13 +0800

    Update period.vue

--- vue/src/views/sys/period.vue
+++ vue/src/views/sys/period.vue
@@ -66,7 +66,7 @@
 
       <el-form-item label="选项">
         <el-radio v-model="tempPeriod.type"  label="0">正选</el-radio>
-        <el-radio v-model="tempPeriod.type" label="1">补选</el-radio>
+        <!--<el-radio v-model="tempPeriod.type" label="1">补选</el-radio>-->
       </el-form-item>
       <el-form-item label="备注">
         <el-input type="text" v-model="tempPeriod.comments">
@@ -76,13 +76,54 @@
     <div slot="footer" class="dialog-footer footer-left" >
       <el-button type="success" @click="setPeriod">发 布</el-button>
     </div>
+
+    <el-table :data="list" v-loading.body="listLoading" style="margin-top: 10px; " element-loading-text="拼命加载中" border fit
+              highlight-current-row>
+      <el-table-column align="center" label="教师选课周期"  >
+        <template slot-scope="scope">
+          {{scope.row.teacherStartDate | formatDate}} -- {{scope.row.teacherEndDate|formatDate}}
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="学生选课周期" prop="nickname" style="width: 60px;">
+        <template slot-scope="scope">
+          {{scope.row.pickStartDate | formatDate}} -- {{scope.row.pickEndDate | formatDate}}
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="学生缴费周期" prop="nicknameCn" style="width: 60px;">
+        <template slot-scope="scope">
+          {{scope.row.feeStartDate | formatDate}} -- {{scope.row.feeEndDate | formatDate}}
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="财务审核费用周期" prop="email" style="width: 60px;">
+        <template slot-scope="scope">
+          {{scope.row.financeStartDate | formatDate}} -- {{scope.row.financeEndDate | formatDate}}
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="结果公示开始时间" prop="email" style="width: 60px;">
+        <template slot-scope="scope">
+          {{scope.row.noticeStartDate | formatDate}}
+        </template>
+      </el-table-column>
+      <!--<el-table-column align="center" label="选项" width="100">-->
+        <!--<template slot-scope="scope">-->
+          <!--<el-tag type="success" v-text="scope.row.type" v-if="scope.row.type===0">正</el-tag>-->
+          <!--<el-tag type="primary" v-text="scope.row.type" v-else>补</el-tag>-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+      <el-table-column align="center" label="备注" prop="comments" width="170"></el-table-column>
+
+    </el-table>
+
   </div>
 </template>
 <script>
+
+  import {formatDate} from '@/utils/index' //
   export default {
     name: 'peroid',
     data() {
       return {
+        list: [],//表格的数据
         listLoading: false,//数据加载等待动画
         tempPeriod: {
           teacherPeriod: "",
@@ -95,7 +136,30 @@
         },
       }
     },
+    filters:{
+      formatDate(time){
+        let date = new Date(time);
+        return formatDate(date,'yyyy-MM-dd hh:mm');
+        //此处formatDate是一个函数，将其封装在common/js/date.js里面，便于全局使用
+      }
+    },
+    created() {
+      this.getList();
+    },
     methods: {
+      getList() {
+        //查询列表
+        this.listLoading = true;
+        this.api({
+          url: "/sys/listPeriod",
+          method: "get",
+          params: this.listQuery
+        }).then(data => {
+          this.listLoading = false;
+          this.list = data.list;
+        })
+      },
+
       setPeriod() {
         this.listLoading = true;
         //保存新文章
