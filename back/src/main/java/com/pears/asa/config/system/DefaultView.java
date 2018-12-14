7e34c24e1d2b05afc07e79c6f0ec046fbafe8145
SunShine <07033320a@gmail.com>
2018-12-14 12:42:00 +0800

    Update DefaultView.java

--- back/src/main/java/com/pears/asa/config/system/DefaultView.java
+++ back/src/main/java/com/pears/asa/config/system/DefaultView.java
@@ -1,7 +1,10 @@
 package com.pears.asa.config.system;
 
+import com.pears.asa.config.i18n.MyLocaleResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -17,5 +20,11 @@ public class DefaultView extends WebMvcConfigurerAdapter {
         registry.addViewController("/").setViewName("forward:/index.html");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
         super.addViewControllers(registry);
+    }
+
+    //注册国际化组件
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new MyLocaleResolver();
     }
 }
